24727b00c889bef52174ce3858095a74f437f1b9
srinivasanhck <srinivasan@cleankoding.com>
2025-05-12 11:19:57 +0530

    small changed node in app

--- frontend/lib/utils/constants.ts
+++ frontend/lib/utils/constants.ts
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:8080"
+// export const API_URL = "http://localhost:8080"
+// export const API_URL = "https://misogi-projectshelf-pcip37u4h-srinivas831s-projects.vercel.app/api"
+export const API_URL = "https://misogi-projectshelf.vercel.app"
